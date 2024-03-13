a7e8851b099750c6d13299034530b0543605d3e1
Chris Vaughn <chris@cvaughn.com>
2024-03-13 16:51:34 -0500

    update examples with labels

--- examples/worker-example.php
+++ examples/worker-example.php
@@ -8,8 +8,8 @@ use FaktoryQueue\Socket;
 
 // $client = new FaktoryClient('faktory', '7419', 'insecure_password'); // Example with password
 $client = new FaktoryClient(Socket::class, 'localhost', '7419', null, true);
-$worker = new FaktoryWorker($client);
-$worker->setQueues(array("default", "monolith.email"));
+$worker = new FaktoryWorker($client, null, ["testing", "example"]);
+$worker->setQueues(array("default", "another.queue"));
 
 $worker->register('cooljob', function ($job) {
     echo "something cool: " . $job['args'][0] . ' ' . $job['args'][1] . "\n";
